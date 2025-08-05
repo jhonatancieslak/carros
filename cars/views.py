668171a0ba3618e6d969f9211dc1fb10af7fe9d0
Jhonatan <jhonatan.ciesla94@gmail.com>
2025-08-05 06:47:30 -0300

    Create page New car and functions

--- cars/views.py
+++ cars/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarForm
 
 def cars_view(request):
     cars = Car.objects.all()
-
-    return render(request, 
-                  'cars.html',
-                  {'cars': cars}
-                  )
+    return render(request, 'cars.html', {'cars': cars})
 
 def new_car_view(request):
-    new_car_form = CarForm()
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list') 
+    else:
+        new_car_form = CarForm()
+    
     return render(request, 'new_car.html', { 'new_car_form': new_car_form })
