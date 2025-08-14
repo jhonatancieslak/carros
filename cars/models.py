55da87474196815dd0c6e81b141a07c25dd9174e
Jhonatan <jhonatan.ciesla94@gmail.com>
2025-08-14 20:25:13 -0300

    Finish App cars

--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -8,7 +9,7 @@ class Brand(models.Model):
         return self.name
 
 
-class Car(models.Model): 
+class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
@@ -17,7 +18,19 @@ class Car(models.Model):
     plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
     photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
-    #Aqui vamos retornar o modelo do carro
     def __str__(self):
         return self.model
+
+
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f'{self.cars_count} - {self.cars_value}'
