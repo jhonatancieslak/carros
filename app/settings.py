0774e044e40b9d9147742a7dddfc9a71d8a8882c
Jhonatan <jhonatan.ciesla94@gmail.com>
2025-08-16 16:25:58 -0300

    Postgres Ajust

--- app/settings.py
+++ app/settings.py
@@ -75,21 +75,17 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'carros',
         'USER': 'postgres',
-        'PASSWORD': 'djangomaster',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
-    },
-    'dev': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
--- openai_api/client.py
+++ openai_api/client.py
@@ -1,26 +1,20 @@
 from openai import OpenAI
 
-
-client = OpenAI(
-    api_key='API_KEY'
-)
-
+client = OpenAI(api_key='API_KEY')
 
 def get_car_ai_bio(model, brand, year):
-    message = ''''
-    Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. Fale coisas específicas desse modelo.
+    message = """
+    Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. 
+    Fale coisas específicas desse modelo.
     Descreva especificações técnicas desse modelo de carro.
-    '''
-    message = message.format(brand, model, year)
+    """.format(brand, model, year)
+
     response = client.chat.completions.create(
-        messages=[
-            {
-                'role': 'user',
-                'content': message
-            }
-        ],
-        max_tokens=1000,
         model='gpt-3.5-turbo',
+        messages=[
+            {'role': 'user', 'content': message}
+        ],
+        max_tokens=1000
     )
 
-    return response.choices[0].message.content
+    return response.choices[0].message["content"]
